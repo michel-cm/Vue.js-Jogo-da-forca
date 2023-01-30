60ba45c12e34ac8ae618cb3c94b6a22e1f45f75c
Michel Correa <58608300+michel-cm@users.noreply.github.com>
2023-01-30 15:04:51 -0300

    Base app/imgs

--- src/App.vue
+++ src/App.vue
@@ -1,5 +1,11 @@
 <template>
-  <div id="app"></div>
+  <div id="app">
+    <h1>Jogo da Forca WDEV</h1>
+
+    <section class="inicio" v-if="tela === 'inicio'">Inicio</section>
+
+    <section class="jogo" v-if="tela === 'jogo'">jogo</section>
+  </div>
 </template>
 
 <script>
@@ -7,8 +13,22 @@ import "./css/global.css";
 
 export default {
   name: "App",
+  data() {
+    return {
+      tela: "inicio",
+    };
+  },
   components: {},
 };
 </script>
 
-<style></style>
+<style>
+#app {
+  width: 100%;
+  height: 100%;
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  justify-content: center;
+}
+</style>
